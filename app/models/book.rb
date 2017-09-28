9570278a31723ac6e5d5ff42095375c2842f7477
Qiangsheng Wang <wangqsh999@gmail.com>
2017-09-28 19:49:18 +0800

    add travis ci

---
+++ app/models/book.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  created_at :datetime         not null
+#  id         :integer          not null, primary key
+#  name       :string
+#  updated_at :datetime         not null
+#
+
+class Book < ApplicationRecord
+end
